4aaddf0c84c6607c1fc209dc5bc776a32cda6135
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-02-25 15:48:50 +0100

    Add command completter for winget.

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -27,6 +27,17 @@ Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName
 
 Import-Module npm-completion
 
+# Command completter for the winget package manager.
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+      $Local:ast = $commandAst.ToString().Replace('"', '""')
+      winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+      }
+}
+
 function Get-Youtube-Music($url) { youtube-dl --extract-audio --audio-format vorbis --audio-quality 3 --output "%(title)s.%(ext)s" $url }
 
 function Get-FolderSize {
